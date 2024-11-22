60891bcb990fe62532afc941a64dbe6718d46bd1
kris <kristoforusrp@gmail.com>
2024-11-22 18:49:56 +0700

    test

--- calc_nth_term.c
+++ calc_nth_term.c
@@ -11,7 +11,7 @@ int recurse(int n, int a, int b, int c) {
 }
 
 int find_nth_term(int n, int a, int b, int c) {
-    printf("n -> %d\n", n);
+    // random comment
     if (n <= 0) {
         return 0;
     }
